34af09e8cfa00ddcaaf5707f63496513b034c2bd
2181031h <2181031h@stu.kobe-u.ac.jp>
2022-10-11 11:53:54 +0900

    Change section1-1

--- section1-1/sketch.js
+++ section1-1/sketch.js
@@ -3,13 +3,18 @@ function setup() {
 }
 
 function draw() {
-  background(0);
-  strokeWeight(4);
-  fill(178, 100, 162);
+  background(255);
+  strokeWeight(0);
+  fill(129, 49, 176);
   triangle(12,120,116,120,116,10);
-  fill(94, 185, 84);
-  triangle(0,108,106,108,106,0);
   fill(255);
+  triangle(6,114,111,114,111,5);
+  strokeWeight(2);
+  stroke(250, 180, 234);
+  fill(255);
+  triangle(0,108,106,108,106,0);
+  strokeWeight(0);
+  fill(250, 180, 234);
   textSize(32);
   textFont("serif");
   text("46", 68, 100);
