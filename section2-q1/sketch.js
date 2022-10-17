c10fb46fd67e1370776f6bf6f06b3b4fb8afba52
2181031h <2181031h@stu.kobe-u.ac.jp>
2022-10-17 20:51:52 +0900

    Change section2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,20 @@
 // 小手調べ
+function setup() {
+  createCanvas(100,100);
+}
+
 function setup(){
   for(let i = 0; i < 10; i++){
+    if(i <= 5){
+      noFill();
+      stroke(0, 0, 255)
+      ellipse(50, 50, i*10+10);  
+    }
+    else{
+      noFill();
+      stroke(255, 0, 0);
+      ellipse(50, 50, i*10+10);
+    }
     // BLANK[1]
   }
 }
